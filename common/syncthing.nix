ecccc2e114daa0aef43bb5be00d186beffa461cd
jefaturico <jefaturico@gmail.com>
2026-06-25 18:40:18 +0200

    hardcoded odin syncthing id

--- common/syncthing.nix
+++ common/syncthing.nix
@@ -1,37 +1,26 @@
 { config, lib, ... }:
 let
-  odinSyncthingIdPath = ../hosts/odin/syncthing-device-id.nix;
-  hasOdinSyncthingId = builtins.pathExists odinSyncthingIdPath;
-
   syncthingDevices = {
     galileo.id = "OQUT2EC-CERWR4S-PJCPFUU-BUMZJZL-EVH66K6-I2YKDX3-6A7RS3I-Z3LLFAW";
     coriolis.id = "YAYGCCW-R4IWEUH-7L7QRYG-TSHGH6K-XOCADP3-3OPOXU3-JAFFTC5-76TF6AO";
     ekman.id = "2STWXA4-JBLZ5FM-ZDFPSR2-VE63IJP-SI4LVAW-ECSVLL4-PRFI27E-6VELQQG";
-  }
-  // lib.optionalAttrs hasOdinSyncthingId {
-    odin.id = import odinSyncthingIdPath;
+    odin.id = "K5ROMZZ-E7WR4MM-EYYOLC5-UNOSCJ2-IIMYRGH-LOQOBZZ-DAV3EK6-CVDZ6AG";
   };
 
   hub = "odin";
-  desktopHosts = [
-    "galileo"
-    "ekman"
-  ];
   spokeHosts = [
     "galileo"
     "ekman"
     "coriolis"
   ];
 
-  syncPeers =
-    if config.networking.hostName == hub then
-      spokeHosts
-    else if hasOdinSyncthingId then
-      [ hub ]
-    else
-      desktopHosts;
+  syncPeers = if config.networking.hostName == hub then spokeHosts else [ hub ];
 
-  availableSyncPeers = builtins.filter (device: device != config.networking.hostName) syncPeers;
+  availableSyncPeers = builtins.filter (
+    device: device != config.networking.hostName && builtins.hasAttr device syncthingDevices
+  ) syncPeers;
+
+  enabledSyncthingDevices = lib.genAttrs availableSyncPeers (device: syncthingDevices.${device});
 
   mkFolder = id: path: {
     inherit id path;
@@ -47,7 +36,7 @@ in
     overrideDevices = true;
     overrideFolders = true;
     settings = {
-      devices = builtins.removeAttrs syncthingDevices [ config.networking.hostName ];
+      devices = enabledSyncthingDevices;
       folders = {
         projects = mkFolder "4pmxv-syxrh" "/home/jefaturico/projects";
         calcurse = mkFolder "5rdcv-wpnjr" "/home/jefaturico/.local/share/calcurse";
